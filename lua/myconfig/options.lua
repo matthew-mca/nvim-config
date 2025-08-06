b99ddf8e4ddcfc315a1defb67a80cd1f2d2a4c0d
Matthew McAteer <MatthewMcA2001@gmail.com>
2025-08-06 01:33:17 +0100

    Add conditional colorcolumn settings

--- lua/myconfig/options.lua
+++ lua/myconfig/options.lua
@@ -16,3 +16,17 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+
+-- Set colorcolumns for select file types.
+-- autocmd ref: https://neovim.io/doc/user/api.html#nvim_create_autocmd()
+local function set_cc_for_filetype(filetype_pattern, columns)
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = filetype_pattern,
+		callback = function()
+			vim.opt.colorcolumn = columns
+		end,
+	})
+end
+
+set_cc_for_filetype("*", "")
+set_cc_for_filetype("python", "72,120")
